f5dba826dd51597f27d8f9c8531591da550c4e81
shichibukaisc <shichibukai_sc@icloud.com>
2015-04-24 22:59:11 -0700

    Project Creations

---
+++ TomcatWindows/README.md
@@ -0,0 +1,4 @@
+# TomcatWindows
+
+TODO: Enter the cookbook description here.
+
---
+++ TomcatWindows/metadata.rb
@@ -0,0 +1,8 @@
+name             'TomcatWindows'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures TomcatWindows'
+long_description 'Installs/Configures TomcatWindows'
+version          '0.1.0'
+
